926ce15c41a49d27e1e95db4ea83025f9e65b66a
vanshverma721 <vvv.721781@gmail.com>
2025-04-03 15:56:24 +0530

    added react code editor

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,17 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+import "prismjs/themes/prism-tomorrow.css"
+import Editor from "react-simple-code-editor"
+import prism from "prismjs"
 import './App.css'
 
 function App() {
+
+  const [code, setcode] = useState(`function sum() {
+  return 1 + 1
+}`)
+
+  useEffect(() => {
+    prism.highlightAll()
+  })
 
   return (
     <>
       <main>
 
         <div className="left">
-          <div className="code"></div>
+
+          <div className="code">
+            <Editor
+              value={code}
+              onValueChange={code => setcode(code)}
+              highlight={code => prism.highlight(code, prism.languages.javascript, "javascript")}
+              padding={10}
+              style={{
+                fontFamily: '"Fira code", "Fira Mono", monospace',
+                fontSize: 16,
+                border: "1px solid #ddd",
+                borderRadius: "5px",
+                height: "100%",
+                widows: "100%"
+              }}
+            />
+          </div>
+
           <div className="review-button">Review</div>
+
         </div>
 
         <div className="right"></div>
